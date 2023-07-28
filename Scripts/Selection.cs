6bde5f96b2f0be244d42f23520ba214e6aeb2d49
Anoy Roy Chowdhury <anoyroyc3545@gmail.com>
2023-07-29 02:02:22 +0530

    Added iTween, models and Pointer selection script

---
+++ Scripts/Selection.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.EventSystems;
+
+public class Selection : MonoBehaviour
+{
+    public Material selectionMaterial;
+
+    private Material originalMaterial;
+   
+    private Transform selection;
+    private RaycastHit raycastHit;
+
+    private MovementController movementController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movementController = GameObject.FindGameObjectWithTag("Player").GetComponent<MovementController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+            if(Physics.Raycast(ray, out raycastHit))
+            {
+                if(selection != null && selection != raycastHit.transform && raycastHit.transform.CompareTag("Markers"))
+                {
+                    selection.GetComponent<Renderer>().material = originalMaterial;
+                    Debug.Log(selection.name);
+                }
+                if(selection == raycastHit.transform || !raycastHit.transform.CompareTag("Markers")) return;
+                selection = raycastHit.transform;
+                TriggerSelection();
+            }
+        }
+    }
+
+    void TriggerSelection()
+    {
+        originalMaterial = selection.GetComponent<Renderer>().material;
+        selection.GetComponent<Renderer>().material = selectionMaterial;
+        movementController.triggerMovement(selection);
+    }
+}
